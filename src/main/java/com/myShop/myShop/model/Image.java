f363c448c707dc0451e472ff6c9dbe8c5dd8ac10
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-01-24 00:41:59 +0600

    product, category and image entity setup and relation setup done

--- src/main/java/com/myShop/myShop/model/Image.java
+++ src/main/java/com/myShop/myShop/model/Image.java
@@ -1,4 +1,30 @@
 package com.myShop.myShop.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+
+
+    @Lob
+    private Blob image;
+    private  String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    public Product product;
 }
