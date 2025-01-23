21be00f3dd395fd00d118d636398ac0a771131cd
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-01-24 01:12:03 +0600

    database setup done

--- src/main/java/com/myShop/myShop/model/Product.java
+++ src/main/java/com/myShop/myShop/model/Product.java
@@ -24,7 +24,7 @@ public class Product {
     private int inventory;
     private  String description;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "category_id")
     private  Category category;
 
